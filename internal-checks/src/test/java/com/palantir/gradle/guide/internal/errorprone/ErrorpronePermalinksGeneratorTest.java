ccb7dd9cad81ed98c0e793febe272458e082a5ef
Callum Rogers <callumr@palantir.com>
2025-02-20 15:33:20 +0000

    Use test error prone

--- internal-checks/src/test/java/com/palantir/gradle/guide/internal/errorprone/ErrorpronePermalinksGeneratorTest.java
+++ internal-checks/src/test/java/com/palantir/gradle/guide/internal/errorprone/ErrorpronePermalinksGeneratorTest.java
@@ -18,14 +18,25 @@ package com.palantir.gradle.guide.internal.errorprone;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.palantir.gradle.guide.errorprone.ConfigurationAvoidanceRegistration;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.BugPattern.SeverityLevel;
+import com.palantir.gradle.guide.errorprone.GradleGuideBugChecker;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 class ErrorpronePermalinksGeneratorTest {
+    @SuppressWarnings("BugCheckerAutoService")
+    @BugPattern(severity = SeverityLevel.ERROR, summary = "Summary")
+    private static final class TestErrorProne extends GradleGuideBugChecker {
+        @Override
+        public MoreInfoLink moreInfoLink() {
+            return new MoreInfoHeadingLink("test.md", "Test Heading");
+        }
+    }
+
     @Test
     void check_a_correct_errorprones_md_is_created() {
-        assertThat(ErrorpronePermalinksGenerator.generate(Set.of(new ConfigurationAvoidanceRegistration())))
+        assertThat(ErrorpronePermalinksGenerator.generate(Set.of(new TestErrorProne())))
                 .isEqualTo(
                         """
                 # gradle-guide Error Prone Permalinks
@@ -42,14 +53,14 @@ class ErrorpronePermalinksGeneratorTest {
                 <tr>
                 <td>
 
-                <a id="ConfigurationAvoidanceRegistration">`ConfigurationAvoidanceRegistration`</a>
+                <a id="TestErrorProne">`TestErrorProne`</a>
 
                 </td>
                 <td>
-                When registering a new `Task`, `Configuration` or other Gradle domain type, use `.register` instead of `.create` to avoid realising the object eagerly and performing unnecessary work which will slow down the build.
+                Summary
                 </td>
                 <td>
-                <a href="guide/avoiding-unnecessary-configuration.md#lazy-task-registration">More Info</a>
+                <a href="guide/test.md#test-heading">More Info</a>
                 </td>
                 </tr>
                 </tbody>
