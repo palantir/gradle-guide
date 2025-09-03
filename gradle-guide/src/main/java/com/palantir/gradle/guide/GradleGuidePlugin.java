fa961063d7b0ec037cbe4f09ef69331f964bb3b7
Kelvin Ou <58218998+kelvinou01@users.noreply.github.com>
2025-09-03 15:22:06 +0100

    Autofix non-abstract tasks (#198)
    Autofix non-abstract tasks

--- gradle-guide/src/main/java/com/palantir/gradle/guide/GradleGuidePlugin.java
+++ gradle-guide/src/main/java/com/palantir/gradle/guide/GradleGuidePlugin.java
@@ -24,8 +24,11 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
 public class GradleGuidePlugin implements Plugin<Project> {
-    private static final Set<String> PATCH_CHECKS =
-            Set.of("ConfigurationAvoidanceRegistration", "ProviderGet", "IllegalMethodCalledDuringTaskExecution");
+    private static final Set<String> PATCH_CHECKS = Set.of(
+            "ConfigurationAvoidanceRegistration",
+            "ProviderGet",
+            "NonAbstractGradleType",
+            "IllegalMethodCalledDuringTaskExecution");
 
     @Override
     public final void apply(Project rootProject) {
